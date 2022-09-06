19ed544fbcff6e15f0cbf98259c8ee1a48cd7760
Alex Chew <alex_chew@msn.com>
2022-09-06 10:58:21 +0800

    bug fix

--- js/info.js
+++ js/info.js
@@ -218,12 +218,13 @@ function saveFeaturedItem(eventId, brokerId, groupType, groupId, groupName,itemT
   var q = "insert into ilife.features values ('"+eventId+"','"+brokerId+"','"+groupType+"','"+groupId+"','"+groupName+"','"+itemType+"','"+itemKey+"','"+jsonStr.replace(/'/g, "’")+"','"+status+"',now())";
   console.log("try to save featured item.",q);
   jQuery.ajax({
-    url:app.config.analyze_api+"?query="+encodeURIComponent(q),
+    url:app.config.analyze_api+"?query=",//+encodeURIComponent(q),
     type:"post",
-    //data:{},
+    data:q, //将具体操作放在数据内
     headers:{
-      "Authorization":"Basic ZGVmYXVsdDohQG1AbjA1"
-      //"Authorization":app.config.ck_options.auth
+        "content-type": "text/plain; charset=utf-8", // 直接提交raw数据
+        "Authorization":"Basic ZGVmYXVsdDohQG1AbjA1"
+        //"Authorization":app.config.ck_options.auth
     },         
     success:function(json){
       console.log("===featured item saved.===\n",json);
